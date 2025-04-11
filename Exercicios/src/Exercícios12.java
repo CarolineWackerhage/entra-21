4ef354dab2a7df5fcde09e9ca6ae69e0e74fff05
CAROLINE NARDELLI WACKERHAGE <caroline.wackerhage@alunos.sc.senac.br>
2025-04-11 20:52:01 -0300

    adicionando exercício 12

---
+++ Exercicios/src/Exercícios12.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Exercícios12 {
+    public static void main(String[] args) {
+
+        // 12. Soma de elementos em array lista
+        ArrayList <Integer> lista = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        System.out.println(lista.get(0) + lista.get(1) + lista.get(2) + lista.get(3) + lista.get(4) );
+
+
+
+
+    }
+    }
