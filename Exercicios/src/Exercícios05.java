5a96d6a124f753bf59e67e739809ea089f4f473a
CAROLINE NARDELLI WACKERHAGE <caroline.wackerhage@alunos.sc.senac.br>
2025-04-11 20:40:01 -0300

    Adicionando exercício 05

---
+++ Exercicios/src/Exercícios05.java
@@ -0,0 +1,15 @@
+public class Exercícios05 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+
+        // 5. Números impares
+
+        int n = 11;
+        int i = 1;
+        while (i <= n ) {
+            System.out.println(i);
+        i += 2;
+        }
+        }
+    }
