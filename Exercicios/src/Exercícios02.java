03f1e4b1475220123dffae0213d04194bf9387be
CAROLINE NARDELLI WACKERHAGE <caroline.wackerhage@alunos.sc.senac.br>
2025-04-11 20:03:29 -0300

    Adicionando exercícios 2

---
+++ Exercicios/src/Exercícios02.java
@@ -0,0 +1,17 @@
+public class Exercícios02 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+
+        // 2. Soma de 1 a N
+
+        int n = 5;
+        int soma = 0;
+            for (int i = 0; i<= n; i++) {
+               soma += i;
+
+                System.out.println(soma);
+
+            }
+    }
+}
