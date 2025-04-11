35257f54429c70ade310d096bc95875be93d8054
CAROLINE NARDELLI WACKERHAGE <caroline.wackerhage@alunos.sc.senac.br>
2025-04-11 20:48:35 -0300

    Adicionando exercício 10

---
+++ Exercicios/src/Exercícios10.java
@@ -0,0 +1,17 @@
+public class Exercícios10 {
+    public static void main(String[] args) {
+
+// 10. contagem regressiva
+
+        int contagem = 10;
+            for ( int i = contagem; i >= 1; i--) {
+                System.out.println( i + "...");
+                Thread.sleep(1000);
+
+            }
+
+
+
+
+    }
+    }
