0b7db2a2f39f940b4a9423a270637daa6fe70083
CAROLINE NARDELLI WACKERHAGE <caroline.wackerhage@alunos.sc.senac.br>
2025-04-11 20:37:34 -0300

    Adicionando exercício 04

---
+++ Exercicios/src/Exercícios04.java
@@ -0,0 +1,17 @@
+public class Exercícios04 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+
+        // 4. Números pares
+
+        int n = 20;
+
+        for (int i = 0; i<= n; i++) {
+            if (i % 2 == 0) {
+                System.out.println("numeros pares: " + i);
+
+            }
+        }
+    }
+}
