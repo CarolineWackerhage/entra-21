f9c57130e32dc6731cb62ba6ebd6951187847c79
CAROLINE NARDELLI WACKERHAGE <caroline.wackerhage@alunos.sc.senac.br>
2025-04-11 20:24:29 -0300

    Adicionando exercício 03

---
+++ Exercicios/src/Exercícios03.java
@@ -0,0 +1,13 @@
+public class Exercícios03 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+
+        // 3. Tabuada de um número
+
+        int tab = 6;
+            for (int i = 0; i <= 10; i++) {
+                System.out.println(tab + "x" + i + "=" + (tab * i));
+            }
+    }
+}
