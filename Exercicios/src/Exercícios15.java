7894bab5abfd50d8672f31872d6acd47ce933b6f
CAROLINE NARDELLI WACKERHAGE <caroline.wackerhage@alunos.sc.senac.br>
2025-04-11 20:58:57 -0300

    Adicionando exercício 15

---
+++ Exercicios/src/Exercícios15.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Exercícios15 {
+    public static void main(String[] args) {
+
+
+        //15. Média valores Array
+
+        ArrayList <Integer> lista = new ArrayList<>(Arrays.asList(1, 10, 3));
+        int soma =  lista.get(0) + lista.get(1) + lista.get(2);
+        System.out.println(soma / 3);
+
+
+
+
+
+
+
+
+
+    }
+    }
