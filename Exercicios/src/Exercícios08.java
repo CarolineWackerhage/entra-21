a7551d812cad2440916a2c5d07cb8513088ec4b2
CAROLINE NARDELLI WACKERHAGE <caroline.wackerhage@alunos.sc.senac.br>
2025-04-11 20:45:54 -0300

    Adicionando exercício 08

---
+++ Exercicios/src/Exercícios08.java
@@ -0,0 +1,21 @@
+public class Exercícios08 {
+    public static void main(String[] args) {
+
+
+        // 8. Númenos primos
+
+        int num = 6;
+            for (int i = 2; i < num; i++) {
+             if (num % i == 0) {
+                 System.out.println("Não é primo");
+             }
+            }
+        System.out.println("É primo");
+
+
+            // Código com Erro
+
+
+
+    }
+    }
