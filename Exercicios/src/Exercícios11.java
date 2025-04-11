4acc1bdde032dba8416c9603d8a8a8cd41a2d35c
CAROLINE NARDELLI WACKERHAGE <caroline.wackerhage@alunos.sc.senac.br>
2025-04-11 20:50:08 -0300

    Adicionando exercício 11

---
+++ Exercicios/src/Exercícios11.java
@@ -0,0 +1,13 @@
+public class Exercícios11 {
+    public static void main(String[] args) {
+
+        // 11. Preenchendo um arrayLista
+
+        ArrayList <Integer> lista = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        System.out.println(lista);
+
+
+
+
+    }
+    }
