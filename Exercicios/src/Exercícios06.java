909bc397bb9f1eabe8de070376d56f519da173a6
CAROLINE NARDELLI WACKERHAGE <caroline.wackerhage@alunos.sc.senac.br>
2025-04-11 20:42:40 -0300

    Adicionando exercício 06

---
+++ Exercicios/src/Exercícios06.java
@@ -0,0 +1,18 @@
+public class Exercícios06 {
+    public static void main(String[] args) {
+
+
+        //6. Fatorial de um número
+
+        int resultado = 1;
+            for (int i = 5; i > 1; i--) {
+                System.out.println( resultado *= i);
+            }
+
+
+
+
+
+
+        }
+    }
