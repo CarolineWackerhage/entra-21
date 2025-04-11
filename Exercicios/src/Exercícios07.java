4d430e12357156da692884b55b192dab3ccfbf24
CAROLINE NARDELLI WACKERHAGE <caroline.wackerhage@alunos.sc.senac.br>
2025-04-11 20:44:08 -0300

    Adicionando exercício 07

---
+++ Exercicios/src/Exercícios07.java
@@ -0,0 +1,20 @@
+public class Exercícios07 {
+    public static void main(String[] args) {
+
+
+        // 7. Soma dos digitos
+
+        int num = 597;
+        int centena = num / 100;
+        int dezena = (num / 10) % 10;
+        int unidade = num % 10;
+
+        System.out.println( centena + dezena + unidade );
+
+
+
+
+
+
+        }
+    }
