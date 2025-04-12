af42cc69a3396236cb4e1c91329a3ae28190cbeb
CAROLINE NARDELLI WACKERHAGE <caroline.wackerhage@alunos.sc.senac.br>
2025-04-11 21:02:37 -0300

    Adicionando exercício 16

---
+++ Exercicios/src/Exercícios16.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Exercícios16 {
+    public static void main(String[] args) {
+
+
+        //   16. Buscar um número
+
+        ArrayList <Integer> lista = new ArrayList<>(Arrays.asList(1, 10, 3));
+            if ( lista.contains(2)) {
+                System.out.println("sim, contém");
+            } else {
+                System.out.println("Não contém");
+            }
+
+
+
+
+
+
+
+
+
+
+    }
+    }
