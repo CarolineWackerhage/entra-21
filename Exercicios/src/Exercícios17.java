14ad5322062781fa638d2b01fa1d7b19c4d7df6b
CAROLINE NARDELLI WACKERHAGE <caroline.wackerhage@alunos.sc.senac.br>
2025-04-11 21:03:59 -0300

    Adicionando exercício 17

---
+++ Exercicios/src/Exercícios17.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Exercícios17 {
+    public static void main(String[] args) {
+
+
+        // 17. Inverter uma lista
+        ArrayList <Integer> lista1 = new ArrayList<>(Arrays.asList(1, 10, 3));
+            for ( int i = lista1.size() - 1; i >= 0; i--) {
+                System.out.println(lista1.get(i));
+            }
+
+
+
+
+
+
+
+
+
+
+    }
+    }
