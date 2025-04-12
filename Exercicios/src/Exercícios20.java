aae9da0d22997875f4ccaae5d1763814754b0c75
CAROLINE NARDELLI WACKERHAGE <caroline.wackerhage@alunos.sc.senac.br>
2025-04-11 21:24:11 -0300

    Adicionando exercício 20

---
+++ Exercicios/src/Exercícios20.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Exercícios20 {
+    public static void main(String[] args) {
+
+
+
+        //20. Ordenar lista crescente
+
+        ArrayList <Integer> lista = new ArrayList<>(Arrays.asList(59, 17 , 4, 25 ));
+        Collections.sort(lista);
+        System.out.println(lista);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    }
