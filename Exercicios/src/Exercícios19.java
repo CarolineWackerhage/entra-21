2b9a093c087f0254dc540c154f4d3f1840fb5eb4
CAROLINE NARDELLI WACKERHAGE <caroline.wackerhage@alunos.sc.senac.br>
2025-04-11 21:18:53 -0300

    Adicionando exercício 19

---
+++ Exercicios/src/Exercícios19.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Exercícios19 {
+    public static void main(String[] args) {
+
+
+
+        // 19. duplicar valores de uma lista
+
+        ArrayList <Integer> lista = new ArrayList<>(Arrays.asList(1, 2 , 3));
+            for ( int i = 0; i < lista.size(); i++) {
+                System.out.println(lista.get(i) * 2);
+            }
+
+
+
+
+
+
+
+
+
+
+
+    }
+    }
